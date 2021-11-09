4cb4e7d44e0fca8aeb3520b70b84c7ef24698504
Tinashe Osewe <t.osewe1@gmail.com>
2021-11-08 19:11:47 -0500

    completed and debugged code for slicing images into cells, straightening images, removing shadows and removing whitespace. Example usage shown in test.m

--- test.m
+++ test.m
@@ -1,16 +1,38 @@
-%% test image slicing
-clear all; close all; clc
+% requires signal processing toolbox
 
-imdata = imread('grid.png');
-imshow(imdata)
-cell1 = getCell(imdata, 1, 1);
+%% image slicing into cells
+clear all; close all; clc
+img = imread('grid.png');
+cell1 = getCell(img, 1, 1);
 imshow(cell1);
 
-%% test grid detection
+%% straightening
 clear all; close all; clc
-imdata = imread('grid.png');
-imdata = rgb2gray(imdata);
-imdata = imrotate(imdata, 10);
-imshow(imdata)
-img = straightenGrid(imdata);
+img = imread('grid2.jpg');
+img = rgb2gray(img);
+img = imrotate(img, -25);
+img = straightenGrid(img);
 imshow(img);
+
+%% shadow removal (may not be necessary)
+clear all; close all; clc
+img = imread('grid2.jpg');
+img = removeShadows(img);
+imshow(img)
+
+%% whitespace removal
+clear all; close all; clc
+img = imread('grid2.jpg');
+img = removeWhitespace(img);
+imshow(img);
+
+%% combined
+clear all; close all; clc
+img = imread('grid2.jpg');
+img = rgb2gray(img);
+img = straightenGrid(img);
+img = removeShadows(img);
+img = removeWhitespace(img);
+figure, imshow(img);
+cell3 = getCell(img, 1, 3);
+figure, imshow(cell3);
