5a2d9964bb04f8fab4d96516bed8fe5bf184352e
Tinashe Osewe <t.osewe1@gmail.com>
2021-11-10 10:26:52 -0500

    trimmed extracted cell containing most recently played move so that border is removed

--- getCell.m
+++ getCell.m
@@ -5,5 +5,10 @@ function [cell] = getCell(grid, x, y)
     [i, j] = size(grid);
     i = floor(i/3);
     j = floor(j/3);
+    % extract cell
     cell = grid(i*(x-1)+1: i*x, j*(y-1)+1: y*j);
+    % crop outer 5% to remove potential borders
+    [height, width] = size(cell);
+    cell = cell(round(width*0.05):round(width*0.95),...
+        round(height*0.05):round(height*0.95));
 end
