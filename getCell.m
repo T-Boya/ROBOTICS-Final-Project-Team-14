a6f29ed76bfd5524d5a98bf8a291220dda9c05bb
Diyanko Bhowmik <diyankobhowmik.db@gmail.com>
2021-11-11 17:33:46 -0500

    Update getCell.m

--- getCell.m
+++ getCell.m
@@ -1,13 +1,13 @@
-% Note: Takes images in grayscale
-% slices image of grid into 9 images of cells
+% Note: Inputs an image in grayscale
+% Slices an image of grid into 9 images containing each cell
 
 function [cell] = getCell(grid, x, y)
     [i, j] = size(grid);
     i = floor(i/3);
     j = floor(j/3);
-    % extract cell
+    % Extracting the cell from the grid
     cell = grid(i*(x-1)+1: i*x, j*(y-1)+1: y*j);
-    % crop outer 5% to remove potential borders
+    % Crop the outer 5% to remove potential borders
     [height, width] = size(cell);
     cell = cell(round(width*0.05):round(width*0.95),...
         round(height*0.05):round(height*0.95));
