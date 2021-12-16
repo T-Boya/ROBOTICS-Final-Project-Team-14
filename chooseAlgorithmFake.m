c79e1d46bc7a8e7110dcbe66468d928ae7a8d527
Tinashe Osewe <t.osewe1@gmail.com>
2021-12-16 17:02:12 -0500

    removed unecessary files, reorganized files

--- chooseAlgorithmFake.m
+++
@@ -1,9 +0,0 @@
-clc;
-p1 = 'We have two algorithms:';
-p2 = '1. Passive algorithm.';
-p3 = '2. Aggressive Algorithm.';
-str = [p1 newline p2 newline p3 newline];
-fprintf(str);
-input('Which do you want to play against? (type 1 or 2): ');
-fprintf('FOOL, you have chosen algorithm: WIN AT ALL COSTS');
-fprintf(newline);
