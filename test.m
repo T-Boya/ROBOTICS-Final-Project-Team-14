ecff0b181a55f9fe93a658ac1dc8af646ae614d0
Tinashe Osewe <t.osewe1@gmail.com>
2021-12-16 20:40:35 -0500

    modified REAME syntax

---
+++ test.m
@@ -0,0 +1,18 @@
+pause(15)
+board = ['O' '_' '_'; '_' '_' '_'; '_' '_' '_']
+pause(1)
+board = ['O' '_' 'X'; '_' '_' '_'; '_' '_' '_']
+pause(1)
+board = ['O' '_' 'X'; '_' '_' '_'; 'O' '_' '_']
+pause(1)
+board = ['O' '_' 'X'; 'X' '_' '_'; 'O' '_' '_']
+pause(1)
+board = ['O' '_' 'X'; 'X' '_' '_'; 'O' '_' 'O']
+pause(1)
+board = ['O' '_' 'X'; 'X' 'X' '_'; 'O' '_' 'O']
+pause(1)
+board = ['O' '_' 'X'; 'X' 'X' 'O'; 'O' '_' 'O']
+pause(1)
+board = ['O' '_' 'X'; 'X' 'X' 'O'; 'O' 'X' 'O']
+pause(1)
+board = ['O' 'O' 'X'; 'X' 'X' 'O'; 'O' 'X' 'O']
